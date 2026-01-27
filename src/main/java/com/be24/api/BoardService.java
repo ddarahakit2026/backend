613034ceafd36a99af050b73e6360a6a8c50ef92
ddarahakit2026 <ddarahakit2026@gmail.com>
2026-01-28 08:52:20 +0900

    [Feat] 싱글톤 디자인 패턴 적용
    BoardService, BoardRepository에 싱글톤 디자인 패턴을 적용해서 실행 중에 객체가 1개만 생성되도록 구현

--- src/main/java/com/be24/api/BoardService.java
+++ src/main/java/com/be24/api/BoardService.java
@@ -3,9 +3,24 @@ package com.be24.api;
 import com.be24.api.model.BoardDto;
 
 public class BoardService {
+
+    // -- BoardService 클래스에 싱글톤 디자인 패턴 적용--
+    private BoardService() {
+
+    }
+
+    private static class SingletonHolder {
+        private static final BoardService instance = new BoardService();
+    }
+
+    public static BoardService getInstance() {
+        return SingletonHolder.instance;
+    }
+    // -- BoardService 클래스에 싱글톤 디자인 패턴 적용--
+
     public BoardDto register(BoardDto dto) {
-        BoardRepository boardRepository = new BoardRepository();
-        // 레포지토리의 결과를 반환받게 변경
+        // BoardRepository 클래스에 싱글톤 적용, new로 생성 못하니까 메소드로 객체 가져옴
+        BoardRepository boardRepository = BoardRepository.getInstance();
         BoardDto returnDto = boardRepository.create(dto);
 
         return returnDto;
