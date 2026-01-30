0aa58923ea36d2ad91405ffec7dfb21b387c7a6b
ddarahakit2026 <ddarahakit2026@gmail.com>
2026-01-30 17:05:24 +0900

    [Refactor] Jwt 관련 기능을 JwtUtil에 옮겼음

--- src/main/java/com/be24/api/user/UserController.java
+++ src/main/java/com/be24/api/user/UserController.java
@@ -7,6 +7,7 @@ import com.be24.api.user.model.UserLoginDtoRes;
 import com.be24.api.user.model.UserSignupDtoRes;
 import com.be24.api.user.model.UserSignupDto;
 import com.be24.api.utils.JsonParser;
+import com.be24.api.utils.JwtUtil;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -35,21 +36,16 @@ public class UserController implements Controller {
         if (req.getRequestURI().contains("signup") && req.getMethod().equals("POST")) {
             UserSignupDto dto = JsonParser.from(req, UserSignupDto.class);
             UserSignupDtoRes returnDto = userService.signup(dto);
-
+            return BaseResponse.success(returnDto);
         } else if (req.getRequestURI().contains("login") && req.getMethod().equals("POST")) {
             UserLoginDto dto = JsonParser.from(req, UserLoginDto.class);
             UserLoginDtoRes returnDto = userService.login(dto);
 
             // 서비스로 부터 정상적으로 Dto를 반환 받으면(로그인 성공하면)
             if(returnDto != null) {
-                String key = "sdfkhgsdkglnhoiurjdfoihgh397478thgwr390289gyrfhp90823uoevbdo823uvh4tf";
-                SecretKey encodedKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(key));
-                String jwt = Jwts.builder()
-                        .claim("idx",returnDto.getIdx())
-                        .claim("email",returnDto.getEmail())
-                        .issuedAt(new Date()).expiration(new Date(System.currentTimeMillis() + 300000)).signWith(encodedKey).compact();
-
-                resp.setHeader("Set-Cookie", "ATOKEN="+jwt + "; Path=/");
+                // JwtUtil에서 토큰 생성 및 확인하도록 리팩토링
+                String token = JwtUtil.createToken(returnDto.getIdx(), returnDto.getEmail());
+                resp.setHeader("Set-Cookie", "ATOKEN="+token + "; Path=/");
                 return BaseResponse.success(returnDto);
             } else {
                 return BaseResponse.fail("로그인 실패");
